c5e13d221e5eaa1e779496c3320fddd91e522cd1
Daniel IKKA <danielik@macbook-pro-de-daniel.local>
2018-04-12 22:35:08 +0200

    Initial Commit

---
+++ La Distrib'/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  La Distrib'
+//
+//  Created by Daniel IKKA on 12/04/2018.
+//  Copyright © 2018 Daniel IKKA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
