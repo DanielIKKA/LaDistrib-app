a74dcd8f869dc3f3011679a351306f7d9402758b
Daniel IKKA <daniel.ikka@edu.ece.fr>
2018-04-29 12:55:13 +0200

    - commencement de la mise en place de l'interface - fix graphic mineur

---
+++ La Distrib'/Main Files/Controllers/StoreViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StoreViewController.swift
+//  La Distrib'
+//
+//  Created by Daniel IKKA on 29/04/2018.
+//  Copyright © 2018 Daniel IKKA. All rights reserved.
+//
+
+import UIKit
+
+class StoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
