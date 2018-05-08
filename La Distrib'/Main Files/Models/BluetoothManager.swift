c7d76772783a0e4e77a90fef4528fd217866781e
Daniel IKKA <daniel.ikka@edu.ece.fr>
2018-05-08 23:33:36 +0200

    - first commit - creation d'un classe BluetoothManager

---
+++ La Distrib'/Main Files/Models/BluetoothManager.swift
@@ -0,0 +1,14 @@
+//
+//  BluetoothManager.swift
+//  La Distrib'
+//
+//  Created by Daniel IKKA on 08/05/2018.
+//  Copyright © 2018 Daniel IKKA. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+class BluetoothManager {
+    
+}
