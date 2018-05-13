f56617190c23f71862b70ce2578c866353f61ed9
Daniel IKKA <daniel.ikka@edu.ece.fr>
2018-05-13 16:22:25 +0200

    commit

--- La Distrib'/Main Files/Models/BluetoothManager.swift
+++ La Distrib'/Main Files/Models/BluetoothManager.swift
@@ -9,6 +9,28 @@
 import Foundation
 import CoreBluetooth
 
-class BluetoothManager {
+class BluetoothManager: CBCentralManager {
     
+//    var centralManager : CBCentralManager!
+    
+}
+
+extension BluetoothManager : CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+            
+        case .unknown:
+            print("central.state is unknown")
+        case .resetting:
+            print("central.state is resetting")
+        case .unsupported:
+            print("central.state is unsupported")
+        case .unauthorized:
+            print("central.state is unauthorized")
+        case .poweredOff:
+            print("central.state is poweredOff")
+        case .poweredOn:
+            print("central.state is poweredOn")
+        }
+    }
 }
