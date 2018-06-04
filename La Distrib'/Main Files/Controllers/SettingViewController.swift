f25a48af155bea19859d9fd56107546abcb077dd
Daniel IKKA <daniel.ikka@edu.ece.fr>
2018-06-04 13:01:32 +0200

    - Credit 10€ avec le bouton Credit de Setting

--- La Distrib'/Main Files/Controllers/SettingViewController.swift
+++ La Distrib'/Main Files/Controllers/SettingViewController.swift
@@ -30,12 +30,31 @@ class SettingViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - IBAction
+    @IBAction func button(_ sender: UIButton) {
+        if sender.tag == 0 {
+            credit()
+        }
+    }
     
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destVc = segue.destination as! HomeViewController
         destVc.currentUser = currentUser
         dataController.saveContext()
+    }
+    
+    private func credit() {
+        
+        
+        let  alertVC = UIAlertController(title: "Credit", message: "Success", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default) { (success) in
+            self.currentUser.balance += 10
+            self.dataController.saveContext()
+        }
+        
+        alertVC.addAction(alertAction)
+        present(alertVC, animated: true)
     }
 
 }
